b25deb24a01a23a998e183e39d6cf515cfe95288
Soyab-Aktar <iamsoyabcode23@gmail.com>
2025-11-16 21:36:23 +0530

    Milestone 2, Module 7 completed

---
+++ Milestone_2/Module_7_(Object_Oriented_Typescript)/src/10_Encapsulation.ts
@@ -0,0 +1,25 @@
+// Encapsulation, the 4th pillar of OOP
+
+class BankAccount {
+  public userId: number;
+  public userName: string;
+  private userBalance: number;
+  protected userUDAI: string;
+  constructor(userId:number, userName:string, userBalance:number, userUDAI:string){
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+    this.userUDAI = userUDAI;
+  }
+  addBalance(addedAmount:number){
+    this.userBalance = this.userBalance + addedAmount;
+  }
+}
+
+class Student extends BankAccount{
+  test(){
+    this.userUDAI;
+  }
+}
+
+const myAccount = new BankAccount(223,'Soyab',1200,'488y44ywe48v');
--- Milestone_2/Module_7_(Object_Oriented_Typescript)/src/9_abstraction.ts
+++ Milestone_2/Module_7_(Object_Oriented_Typescript)/src/9_abstraction.ts
@@ -1,2 +1,41 @@
 // Abstraction, the 3rd pillar of OOP
 
+// interface MediaPlayer{
+//   play() : void;
+//   pause() : void;
+//   stop() : void;
+// }
+// class MusicPlayer implements MediaPlayer{
+//   play(): void {
+//     console.log("Playing music");
+//   }
+//   pause(): void {
+//     console.log("Music Pause");
+//   }
+//   stop(): void {
+//     console.log("Music Stop");
+//   }
+// }
+// const myPlayer = new MusicPlayer();
+// myPlayer.play();
+
+abstract class MediaPlayer{
+  abstract pause():void;
+  abstract play(): void;
+  abstract stop():void;
+}
+
+class myPlayer extends MediaPlayer{
+  play(): void {
+    console.log("Playing music");
+  }
+  pause(): void {
+    console.log("Music Pause");
+  }
+  stop(): void {
+    console.log("Music Stop");
+  }
+}
+
+const soyabPlayer = new myPlayer();
+soyabPlayer.pause();
